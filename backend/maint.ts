bc4415ad36c92586629529fa22754462967416de
cesar0987 <cesarbenitez46@gmail.com>
2024-12-06 17:54:44 -0300

    feat:Create controllers

--- backend/maint.ts
+++ backend/maint.ts
@@ -1,28 +1,76 @@
-import { PrismaClient } from "../backend/generated/client/deno/edge.js";
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { PrismaClient } from "./generated/client/deno/edge.ts";
+import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 
-const envVariables = config({ export: true });
+const envVars = config({ export: true });
+/**
+ * Initialize.
+ */
 
 const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: envVariables.DATABASE_URL,
+      url: envVars.DATABASE_URL,
     },
   },
 });
-
 const app = new Application();
 const router = new Router();
 
-router.get("/", async (ctx) => {
-  const dinosaurs = await prisma.dinosaur.findMany();
-  ctx.response.body = dinosaurs;
-});
+/**
+ * Setup routes.
+ */
+
+router
+  .get("/", (context) => {
+    context.response.body = "Welcome to the Dinosaur API!";
+  })
+  .get("/dinosaur", async (context) => {
+    // Get all dinosaurs.
+    const dinosaurs = await prisma.dinosaur.findMany();
+    context.response.body = dinosaurs;
+  })
+  .get("/dinosaur/:id", async (context) => {
+    // Get one dinosaur by id.
+    const { id } = context.params;
+    const dinosaur = await prisma.dinosaur.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    context.response.body = dinosaur;
+  })
+  .post("/dinosaur", async (context) => {
+    // Create a new dinosaur.
+    const { name, description } = await context.request.body("json").value;
+    const result = await prisma.dinosaur.create({
+      data: {
+        name,
+        description,
+      },
+    });
+    context.response.body = result;
+  })
+  .delete("/dinosaur/:id", async (context) => {
+    // Delete a dinosaur by id.
+    const { id } = context.params;
+    const dinosaur = await prisma.dinosaur.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+    context.response.body = dinosaur;
+  });
+
+/**
+ * Setup middleware.
+ */
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-await app.listen({ port: 3000 });
+/**
+ * Start server.
+ */
 
-console.log("Server running on http://localhost:3000");
+await app.listen({ port: 8000 });
