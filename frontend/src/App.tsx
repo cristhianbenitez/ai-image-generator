e966829ba0b62819d4904af831ee55e7886c21bc
Cristhian Benitez <cristhiantilleria@gmail.com>
2024-12-07 21:27:57 -0500

    feat: Enhance frontend with routing and styling improvements
    - Added `react-router-dom` for client-side routing.
    - Updated Tailwind CSS configuration to include custom colors and font settings.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
+import { Layout } from '@components/Layout';
+import { Home } from '@pages/Home';
+
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">My App</h1>
-    </div>
+    <Layout>
+      <Home />
+    </Layout>
   );
 }
 
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,5 @@
+export const Home = () => {
+  return (
+    <div className="w-full h-full flex items-center justify-center">Home</div>
+  );
+};
